f45355787b4b6192e37246abdedc5924db2da380
Derbis Corrales Varela <derbis.corrales@hotmail.com>
2022-05-24 07:38:20 +0200

    Angular pub

--- UI/Solvtio/src/environments/environment.prod.ts
+++ UI/Solvtio/src/environments/environment.prod.ts
@@ -1,3 +1,41 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+const currentEnvironment = 'dev';
+const serverUrl = `https://localhost:5002`;
+const clientUrl = 'http://localhost:4200';
+
 export const environment = {
-  production: true
+  production: false,
+  currentEnvironment: currentEnvironment,
+  clientUrl: clientUrl,
+  clientRoot: clientUrl,
+  clientId: 'AngularClient',
+  clientScope: ['profile', 'openid', 'angular', 'offline_access'].join(' '),
+  aggregatorUrl: `${serverUrl}/api/v1/`,
+  apiUrl: 'http://localhost:40274/api/',
+  idsUrl: 'https://conecta-development.northeurope.cloudapp.azure.com/api/v1/',
+  ivcertUrl: 'https://demo.ivcert.net/login?returnUrl=%2Fcircuits%2Fnew',
+  stsAuthority: `${serverUrl}`,
+  configuration: {
+    formats: {
+      angularDate: 'dd/MM/yyyy',
+      angularFriendlyDate: 'dd MMMM yyyy',
+      momentDate: 'DD-MMM-YY',
+      angularDateAndTime: 'dd/MM/yy HH:mm',
+      momentDateAndTime: 'DD-MMM-YY HH:mm',
+      serverDateAndTime: 'YYYY/MM/DD HH:mm',
+      serverDate: 'YYYY/MM/DD',
+    },
+    files: {
+      acceptedTypes:
+        '.pdf,.doc,.docx,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+      maxSize: 4e6,
+    },
+  },
+  profile: {
+    languange: 'es',
+    theme: 'dynamic',
+  },
 };
